7c1b49f51469f0bd091e3b33449d2023a794775c
Shivaji <shivaji.chakravorty5@gmail.com>
2026-05-01 21:48:04 +0530

    added prompt token and response token counts

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
 response = client.models.generate_content(
   model="gemini-2.5-pro",
-  contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+  contents = prompt
 )
 
-print(response.text)
+if response.usage_metadata is None:
+    raise RuntimeError("Response is missing usage metadata")
+
+usage_meta = response.usage_metadata
+print(f"User prompt: {prompt}")
+print(f"Prompt tokens: {usage_meta.prompt_token_count}")
+print(f"Response tokens: {usage_meta.total_token_count}")
+print(f"Response:\n{response.text}")
 
 
 
